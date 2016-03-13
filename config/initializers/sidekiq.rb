6ec7bf8f3f1cfb1f0338c5b4d2e6635a260fd150
Troy Anderson <the.inventor@gmail.com>
2016-03-13 13:13:47 -0700

    try sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,9 +3,5 @@ REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.passwor
 
 
 Sidekiq.configure_server do |config|
-  # if Rails.env.production?
-  #   config.redis = { url: ENV['REDISCLOUD_URL'] } #{ENV['REDIS_PORT_6379_TCP_PORT']}/0" }
-  #
-  # end
-
+  config.redis = { url: ENV['REDISCLOUD_URL'] || "redis://localhost:6379/" }
 end
