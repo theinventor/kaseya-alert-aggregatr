33452a9271b947150348eca9f0ca9c28b4ff0cb4
Troy Anderson <the.inventor@gmail.com>
2016-03-13 13:11:53 -0700

    try sidekiq heroku

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,11 @@
+uri = URI.parse(ENV["REDISCLOUD_URL"] || "redis://localhost:6379/" )
+REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+
+
 Sidekiq.configure_server do |config|
-  if Rails.env.production?
-    config.redis = { url: ENV['REDISCLOUD_URL'] } #{ENV['REDIS_PORT_6379_TCP_PORT']}/0" }
-  else
-  end
+  # if Rails.env.production?
+  #   config.redis = { url: ENV['REDISCLOUD_URL'] } #{ENV['REDIS_PORT_6379_TCP_PORT']}/0" }
+  #
+  # end
 
 end
